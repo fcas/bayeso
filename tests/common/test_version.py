a6789c487c50bc93b363e4aeb6eef64a3990b7cb
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-04-30 13:38:35 -0400

    Update a test

--- tests/common/test_version.py
+++ tests/common/test_version.py
@@ -12,9 +12,5 @@ def test_version_bayeso():
     assert bayeso.__version__ == STR_VERSION
 
 def test_version_setup():
-    try:
-        import importlib
-        assert importlib.metadata.version("bayeso") == STR_VERSION
-    except:
-        import pkg_resources
-        assert pkg_resources.require("bayeso")[0].version == STR_VERSION
+    import importlib
+    assert importlib.metadata.version("bayeso") == STR_VERSION
